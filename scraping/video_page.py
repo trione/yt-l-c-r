a3da3d7a66bee27c2eef70de72065f55e1728cc5
trione <godgoodgodwin1996@gmail.com>
2018-06-15 05:08:24 +0900

    Fix scraping

--- scraping/video_page.py
+++ scraping/video_page.py
@@ -1,38 +1,60 @@
 # coding=utf-8
 import httplib2
 import re
+import json
 
-# return str type html page 
+from scraping.item import video_details
+
+page = None
+
 def open_by_id(video_id=None):
-	if video_id is None :
-		return None
+	if video_id is None : return None
 
-	uri = 'https://www.youtube.com/watch?v=' + video_id
 	h = httplib2.Http()
-	
-	try:
-		resp, content = h.request(uri)
-		return content
-	
-	except Exception as e:
-		raise e
+	uri = 'https://www.youtube.com/watch?v=' + video_id
+	headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
 
-
-#return str type OF live_chat_replay's continuation
-def pick_out_chat_continuation(video_id=None):
-	page = open_by_id(video_id=video_id)
-	if page is None: return None
-
+	global page
+	resp, page = h.request(uri=uri, headers=headers)
 	page = page.decode(encoding='utf-8')
 
-	pattern = r'continuation=([a-zA-Z0-9_%]+)'
+	return page
+
+#return str type OF live_chat_replay's continuation
+def pick_out_chat_continuation():
+	global page
+	if page is None: return None
+
+	pattern = r'"continuation":"(op2w0w[a-zA-Z0-9_%]+)"'
 	p = re.compile(pattern)
 	m = p.search(page)
 
-	if m is not None:
-		# print(m.group(1))
-		conti = m.group(1)
-		return conti
-	else :
-		# print("No Match")
+	if m is None: return None
+	
+	conti = m.group(1)
+	return conti
+
+
+def get_video_details():
+	global page
+	if page is None: return None
+
+	pattern = r'({"responseContext":.+})\);'
+	p = re.compile(pattern)
+	m = p.search(page)
+
+	if m is None: return None
+
+	json_str = m.group(1)
+	json_data = json.loads(json_str)
+
+	if "videoDetails" in json_data:
+		details_json = json_data["videoDetails"]
+		microformat_json = None
+		if "microformat" in json_data: 
+			microformat_json = json_data["microformat"]
+		vd = video_details.VideoDetails(details_json, microformat_json)
+		return vd
+
+	else:
 		return None
